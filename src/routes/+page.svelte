809f5dfb88b65558ed6df0401c509281da8f0473
Sidrit Mucaj <SidMuc21@htl-shkoder.com>
2025-02-04 12:20:23 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,41 @@
+<header>
+This is a header
+</header>
+<script>
+let greeting = $state ("Hello");
+</script>
+
+<div>
+    <h1>{greeting}</h1>
+    <h2>What do you want to write?</h2>
+    <input type = "text" bind:value={greeting}>
+    </div>
+
+<footer>This is a footer</footer>
+
+<style>
+div{
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+}
+h1{
+    color: rgb(51, 181, 221);
+    font-size: 50px;
+}
+header, footer{
+    width: 100vw;
+    background-color: rgb(115, 115, 194);
+    padding-top: 50px;
+    padding-bottom: 50px;
+    font-size: 30px;
+    text-align: center;
+    color: rgb(235, 237, 255);
+}
+
+footer{
+  position: fixed;
+  left: 0;
+  bottom: 0;
+}
+</style>
